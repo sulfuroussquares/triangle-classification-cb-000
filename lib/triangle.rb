0916a04b41fc2d79c550a695d0cdaf632fdd689d
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-12 17:56:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,8 @@ class Triangle
     if (side1 < 0 || side2 < 0 || side3 < 0)
       begin
         raise TriangleError
+        rescue TriangleError => error
+          puts error.message
   end
 
   def kind
