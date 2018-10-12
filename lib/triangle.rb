c0b53efcd8f636b30a744219919cfd3aa332b72a
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-12 17:57:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,6 +20,14 @@ class Triangle
       (@side1 == @side2 && @side2 != @side3) ||
       (@side1 == @side3 && @side1 != @side2)
       :isosceles
+    elsif
+      (side1 < 0 || side2 < 0 || side3 < 0)
+        begin
+          raise TriangleError
+          rescue TriangleError => error
+            puts error.message
+          end
+          end #end if
     else
       :scalene
     end # end if
