05926b80b738822b4bedcf57b77cae4e64af93cc
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-12 17:58:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
       (@side1 == @side2 && @side2 != @side3) ||
       (@side1 == @side3 && @side1 != @side2)
       :isosceles
-    elsif (side1 < 0 || side2 < 0 || side3 < 0)
+    elsif (@side1 < 0 || @side2 < 0 || @side3 < 0)
         begin
           raise TriangleError
           rescue TriangleError => error
@@ -38,4 +38,3 @@ class Triangle
       "Illegal triangle property; Please check your values"
     end #end message
   end #end class TriangleError
-
