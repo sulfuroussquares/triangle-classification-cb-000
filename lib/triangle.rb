6427c7a5cebb704ebfc2b0752cc588d083864ffc
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-12 18:02:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,13 @@ class Triangle
   end
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
+    if (@side1 < 0 || @side2 < 0 || @side3 < 0)
+      begin
+        raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+        end #end begin
+  elsif @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif (@side1 != @side2 && @side2 == @side3) ||
       (@side1 == @side2 && @side2 != @side3) ||
